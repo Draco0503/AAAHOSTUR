9206cc98cc5ddd7d4f0b628d13c2090b1c4218f1
Despinosa <despinosa.rc@gmail.com>
2023-05-08 19:41:26 +0200

    SQLS_BIEN

--- aaahostur/app.py
+++ aaahostur/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, jsonify
 from flask_mysqldb import MySQL
 from config import config
 
@@ -14,6 +14,37 @@ def index():
         return '<h1>Prueba</h1>'
     except Exception as ex:
         return "Error"
+    
+@app.route('/listaUsuarios', methods=['GET'])
+def usuarios():
+   try:
+        cursor = mysql.connection.cursor()
+        sql = "SELECT UserName, UserPass FROM users"
+        cursor.execute(sql)
+        #almacena el resultado de la query y lo "traduce" a python
+        datos=cursor.fetchall()
+        print(datos)
+        listaUsuarios=[]
+        #for para recorrer todos los datos sacados y almacenarlos en un objeto 
+        for fila in datos:
+            usuario = {'UserName':fila[0], 'UserPass':fila[1]}
+            #se agrega el usuario a la lista de usuarios
+            listaUsuarios.append(usuario)
+        return jsonify({'usuarios':listaUsuarios, 'mensaje':"Esta es la lista de usuarios"})
+   except Exception as ex:
+         return jsonify({'mensaje':"No funciono el mostrar la lista de usuarios...."})
+   
+@app.route('/registrarUsuario', methods=['POST'])
+def registrarUsuarios():
+   try:
+        cursor = mysql.connection.cursor()
+        sql = f"INSERT INTO users (UserName, UserPass) VALUES('{0}','{1}','{2}')".format(request.json['UserName'], request.json['UserPass'])
+        cursor.execute(sql)
+        mysql.connection.commit()
+        return jsonify({'mensaje':"Usuario Registrado"})
+   except Exception as ex:
+         return jsonify({'mensaje':"Error al registrar...."})
+
 
 #FUNCIONES PARA LOS ERRORES MAS COMUNES
 
--- aaahostur/resources/scripts_create_table/1_Schedule.sql
+++
@@ -1,7 +0,0 @@
-DROP datatable IF EXISTS Schedule;
-CREATE TABLE Schedule(
-ID_SCHEDULE INT AUTO INCREMENT NOT NULL,
-Name VARCHAR(512) NOT NULL,
-Description VARCHAR(512) NOT NULL,
-PRIMARY KEY (ID_SCHEDULE)
-);                                                                                                           .
