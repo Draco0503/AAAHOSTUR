6024f258b2c0112b0976a314ae060823caf52c25
Alex <75042285+Draco0503@users.noreply.github.com>
2023-05-09 09:49:07 +0200

    Script de DB funcional para Linux y Mac

--- aaahosturdb/createDBTables.sh
+++ aaahosturdb/createDBTables.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-host=$1
-user=$3
-passwd=$4
-db=$2
-echo "Paso 0 completado"
+user=$2
+passwd=$3
+db=$1
 
 if [ "$db" = "" ]; then
     echo "Uso: $0 host db_name user password"
     exit 1
 fi
 
-echo "Paso 1, primera comprobacion"
-
-mysql --host=$host --user=$user --password=$passwd -e "drop database if exists $db; create database $db"
-
-echo "Paso 2 completado"
+sudo mysql --user=$user --password=$passwd -e "drop database if exists $db; create database $db"
 
 for sql_file in ./tables/*.sql; do
     echo "Importing $sql_file";
-    mysql --host=$host --user=$user --password=$passwd $db< $sql_file;
+    sudo mysql --user=$user --password=$passwd $db < $sql_file;
 done
