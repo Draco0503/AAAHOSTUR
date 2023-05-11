df0c7f482c950f08d6651ba9260a3dfad51bfbec
Despinosa <despinosa.rc@gmail.com>
2023-05-11 14:07:53 +0200

    4usuarios insertados y metodo registro user(membes/company) en construcción

--- aaahostur/app.py
+++ aaahostur/app.py
@@ -16,41 +16,105 @@ def index():
         return "Error"
     
 @app.route('/listaRoles', methods=['GET'])
-def usuarios():
-   try:
+def roles():
+    try:
         cursor = mysql.connection.cursor()
         sql = "SELECT ID_ROLE, Name, Description FROM Role"
         cursor.execute(sql)
         #almacena el resultado de la query y lo "traduce" a python
         datos=cursor.fetchall()
-        print(datos)
         listaRoles=[]
         #for para recorrer todos los datos sacados y almacenarlos en un objeto 
         for fila in datos:
-            usuario = {'ID_ROLE':fila[0], 'Name':fila[1], 'Description':fila[2]}
+            role = {'ID_ROLE':fila[0], 'Name':fila[1], 'Description':fila[2]}
             #se agrega el usuario a la lista de usuarios
-            listaRoles.append(usuario)
-        return jsonify({'usuarios':listaRoles, 'mensaje':"Esta es la lista de usuarios"})
-   except Exception as ex:
-         return jsonify({'mensaje':"No funciono el mostrar la lista de usuarios...."})
-   
-@app.route('/registrarUsuario', methods=['POST'])
-def registrarUsuarios():
-   try:
+            listaRoles.append(role)
+        return jsonify({'roles':listaRoles, 'mensaje':"Esta es la lista de Roles"})
+    except Exception as ex:
+         return jsonify({'mensaje':"No se pudo ver los roles"})
+    
+@app.route('/verUsuarios', methods=['GET'])
+def usuarios():
+    try:
         cursor = mysql.connection.cursor()
-        sql = f"INSERT INTO users (UserName, UserPass) VALUES('{0}','{1}','{2}')".format(request.json['UserName'], request.json['UserPass'])
+        sql = "SELECT Passwd, Email, id_Role FROM User"
+        cursor.execute(sql)
+        #almacena el resultado de la query y lo "traduce" a python
+        datos=cursor.fetchall()
+        listaUser=[]
+        #for para recorrer todos los datos sacados y almacenarlos en un objeto 
+        for fila in datos:
+            user = {'Passwd':fila[0], 'Email':fila[1], 'id_Role':fila[2]}
+            #se agrega el usuario a la lista de usuarios
+            listaUser.append(user)
+        return jsonify({'Usuarios':listaUser, 'mensaje':"Esta es la lista de usuarios"})
+    except Exception as ex:
+         return jsonify({'mensaje':"No se pudo ver los roles"})
+   
+@app.route('/registrarSuperAdmin', methods=['POST'])
+def registrarSuperAdmin():
+   try:
+        print(request.json)
+        cursor = mysql.connection.cursor()
+        sql = """INSERT INTO User (ID_USER, Passwd, Email, id_Role)
+        VALUES({0}, '{1}', '{2}', {3})""".format(request.json['ID_USER'],
+        request.json['Passwd'], request.json['Email'], request.json['id_Role'])
         cursor.execute(sql)
         mysql.connection.commit()
         return jsonify({'mensaje':"Usuario Registrado"})
    except Exception as ex:
          return jsonify({'mensaje':"Error al registrar...."})
+   
+@app.route('/registrarAdmin', methods=['POST'])
+def registrarAdmin():
+   try:
+        print(request.json)
+        cursor = mysql.connection.cursor()
+        sql = """INSERT INTO User (Passwd, Email, id_Role)
+        VALUES('{0}', '{1}', {2})""".format(request.json['Passwd'], 
+        request.json['Email'], request.json['id_Role'])
+        cursor.execute(sql)
+        mysql.connection.commit()
+        return jsonify({'mensaje':"Usuario Registrado"})
+   except Exception as ex:
+         return jsonify({'mensaje':"Error al registrar...."})
+   
+@app.route('/registrarUsuario', methods=['POST'])
+def registrarUsuario():
+   try:
+        print(request.json)
+        cursor = mysql.connection.cursor()
+        sql = """INSERT INTO User (Passwd, Email, id_Role)
+        VALUES('{0}', '{1}', {2})""".format(request.json['Passwd'], 
+        request.json['Email'], request.json['id_Role'])
+        typeMember = request.json['id_Role']
+        prueba = cursor.execute(sql)
+        print(prueba)
+        sql2 = "SELECT max(ID_USER) FROM User"
+        id = cursor.execute(sql2)    
+        match typeMember:
+            case 104:
+                print("Insertar Miembro")
+                print(id)
+                sql3 = "INSERT INTO Member (ID_MEMBER, Name, Surname) VALUES("+ id + ", '{3}', '{4}')".format(request.json['Name'], request.json['Surname'])
+                cursor.execute(sql3)
+            case 105:
+                print("Insertar Compañia")
+            case _:
+                print("Caso Default")
 
+        mysql.connection.commit()
+        return jsonify({'mensaje':"Usuario Registrado"})
+   except Exception as ex:
+         mysql.connection.rollback()
+         return jsonify({'mensaje':"Error al registrar...."})
+        
 
 #FUNCIONES PARA LOS ERRORES MAS COMUNES
 
 #Error 404-not found
 def pagina_No_Encontrada(err):
-    return "<h1>La página a la que intentas acceder no existe</h1>"
+    return "<h1>La página a la que intentas acceder no existe</h1>", 404
 
 
 
