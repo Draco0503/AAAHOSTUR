38ee28d65c8465a789f26b3c0a676dc32ef334f7
Tecnodwarff <david_ksa01@hotmail.com>
2023-06-02 14:14:52 +0200

    retocando formulario de oferta

--- aaahostur/app.py
+++ aaahostur/app.py
@@ -156,7 +156,7 @@ def gateway_timeout(msg: str = ERROR_504_DEFAULT_MSG) -> Response:
 # method for testing purposes
 @app.route('/prueba')
 def prueba():
-    return render_template('login.html', prueba='holka')
+    return render_template('addoffer.html', prueba='holka')
 
 
 # endregion
